2f46c37a3adaa3929d9240c4d505661aacfd0c08
Kyle <noreply>
2025-03-21 10:48:25 -0400

    cleanup: remove powershell stuff
    Most of this stuff is moved into the windows setup repo

--- powershell/sort_files_into_folders.ps1
+++
@@ -1,9 +0,0 @@
-
-$list = Get-ChildItem -Path '.' -Recurse | Where-Object {$_.PSIsContainer -eq $false}
-
-
-ForEach($n in $list) {
-    #write-host $n.Directory
-    New-Item -Path $n.Directory -Name $n.Basename -ItemType "directory"
-    Move-Item -Path $n -Destination "$($n.Directory)/$($n.Basename)"
-}
