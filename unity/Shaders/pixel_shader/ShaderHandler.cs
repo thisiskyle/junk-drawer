7fb22db928bc2e605dc5ea0d92ba17b0957fcf89
Kyle <noreply>
2025-07-31 20:40:38 -0400

    pixel shader

---
+++ unity/Shaders/pixel_shader/ShaderHandler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderHandler : MonoBehaviour
+{
+    public Material effectMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        source.filterMode = FilterMode.Point;
+        Graphics.Blit(source, destination, effectMaterial);
+    }
+}
